de6032546a0f19fb034fb0522ae60cc0272daeab
Alcadian <norbertalcadian@gmail.com>
2024-01-02 18:56:38 +0000

    redirecting

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ const express = require("express");
 //setting up express app
 const app = express();
 
-//listening for request 
+//listening for request
 app.listen(3000);
 
 //responding
 app.get("/", (req, res) => {
-  res.send("<h1> hello world oooooo </h1>");
+  res.sendFile("/view/index.html", { root: __dirname });
 });
+app.get("/about", (req, res) => {
+  res.sendFile("/view/about.html", { root: __dirname });
+});
+
+//redirects
+app.get('/about-us', (req, res) => {
+  res.redirect('/about');
+});
+
+//404 
+app.use((rep, res) =>{
+  res.status(404).sendFile('./view/404.html', { root: __dirname })
+})
